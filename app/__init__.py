dd44b841733e25dd2c2d76ec325b90e754e0795a
GersenX3 <galdl1438@gmail.com>
2025-09-04 16:51:46 -0600

    [TESTING & FIXING]

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,17 @@ from flask_jwt_extended import JWTManager
 from .db import db
 from .routes import auth_bp
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://authuser:secret@postgres:5432/authdb"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = "super-secret-key"
 
-db.init_app(app)
-JWTManager(app)
-app.register_blueprint(auth_bp, url_prefix="/auth")
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://authuser:secret@postgres:5432/authdb"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "super-secret-key"
+
+    db.init_app(app)
+    JWTManager(app)
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    return app
