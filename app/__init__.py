f81970a5a5fd782f380f159a11e2cc4d8576c6c2
GersenX3 <galdl1438@gmail.com>
2025-09-11 16:12:56 -0600

    [UPDATE] CORS enabled in project

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from flask_jwt_extended import JWTManager
 from .db import db
 from .routes import auth_bp
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://authuser:supersecretpassword@postgres-auth/authdb"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = "super-secret-key"
